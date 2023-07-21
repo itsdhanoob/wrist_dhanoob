58df5ea89dc3a97310999fe41c56b3a94ff05203
itsdhanoob <ben.birmole@gmail.com>
2023-07-21 16:26:06 +0200

    final setup

--- Constants.py
+++ Constants.py
@@ -5,6 +5,6 @@ KP = 0.4        # determines the system's response time (small kp = slow respons
 KD = 0          # improves setteling time and stability by damping overshoot ()
 KI = 1          # reduces the steady state error ie velocity_error, postion_error 
 
-current_threshold = 40
+current_threshold = 30
 
 
