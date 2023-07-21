d0bbe23c74f6829bd0d685c353f8008af6fe2056
itsdhanoob <ben.birmole@gmail.com>
2023-07-21 15:14:54 +0200

    current based limit

--- Constants.py
+++ Constants.py
@@ -1,6 +1,8 @@
 POSITION_OFFSET_DOC = 2048*(4.4/2048)  # convert encoder ticks to radians
 POSITION_OFFSET_PAT = 2048*(2.2/2048) 
 
-KP = 0.4
-KD = 0
-KI = 1
+KP = 0.4        # determines the system's response time (small kp = slow response time, large kp = oscillation)
+KD = 0          # improves setteling time and stability by damping overshoot ()
+KI = 1          # reduces the steady state error ie velocity_error, postion_error 
+
+current_threshold = 2000 
